efc6ad5c2608e159c432a3b0cfa68a504cc66bf8
a1ekP <a1ekc.p85@gmail.com>
2022-08-14 16:01:05 +0300

    add classConstr

---
+++ OOP/classConstr.js
@@ -0,0 +1,41 @@
+class Auto{
+    constructor(name, model, yaer, vin){
+        this.name = name;
+        this.model = model;
+        this.yaer = yaer;
+        this.vin = vin;
+    }
+    log(){
+        return `${this.name} ${this.model}, ${this.year}`
+    }
+    checkVin(){
+        return this.vin === 16 ? true : false;
+    }
+}
+class Auto_Fuel extends Auto{
+    constructor(name, model, yaer, vin, volume, consum){
+        super(name, model, yaer, vin, volume, consum);
+        this.name = name;
+        this.model = model;
+        this.yaer = yaer;
+        this.vin = vin;
+        this.volume = volume;
+        this.consum = consum;
+    }
+    showFuelConsumption(){
+        return `Обьем: ${this.volume}, Расход: ${this.consum}`;
+    }
+}
+class Auto_Electric extends Auto{
+    constructor(name, model, yaer, vin,battery){
+        super(name, model, yaer, vin, battery)
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.vin = vin;
+        this.battery = battery;
+    }
+    showBatteryConfig(){
+        return this.battery;
+    }
+}
